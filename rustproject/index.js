e72eb51ea2927dfdeabea0406fb8a90d5eb54104
Celal Mete <87307659+CelalMete@users.noreply.github.com>
2026-02-24 20:05:50 +0300

    ney

--- rustproject/index.js
+++ rustproject/index.js
@@ -4,10 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     async function baslat() {
         await init();
         const canvas = document.getElementById('img1'); 
+        const buyutec=document.getElementById('buyutecCanvas')
+        const buyutctx=buyutec.getContext('2d')
         const btn1 = document.getElementById('orgin');
         const btn2 = document.getElementById('zoom');
         const btn4 = document.getElementById('buyuk');
         const btn3 = document.getElementById('nokta');
+        const btn5 = document.getElementById('buyutec');
         const ctx = canvas.getContext('2d');
         canvas.width = 670;//genişlik pixel sayisi  1 pixel 4 sayı 
         canvas.height = 500;//yükseklik pixel sayısı
@@ -42,12 +45,20 @@ document.addEventListener("DOMContentLoaded", () => {
                 resmi_guncelle(ctx, canvas, sonucPixels, canvas.width, canvas.height);
             });
             btn4.addEventListener('click', () => {
-                btn = 3;
+                btn = 4;
                console.log(hamVeri)
                 sonucPixels = resmi_olcekle(hamVeri, btn, canvas.width, canvas.height, canvas.width*2, canvas.height*2);
                 console.log(sonucPixels.length)
                 resmi_guncelle(ctx, canvas, sonucPixels, canvas.width*2, canvas.height*2);
             });
+btn5.addEventListener('click', () => {
+                const canvascoor = canvas.getBoundingClientRect();//coordinatları alıyo nası bilmiom
+               canvas.addEventListener('mousemove',function(e){
+                let farex=e.clientX-canvascoor.left;
+                let farey=e.clientY-canvascoor.top;
+                buyutctx.drawImage(image1,farex - 50,farey-50, 100, 100,0,0, buyutec.width, buyutec.height)
+               })
+});
         };
     }
     function resmi_guncelle(ctx, canvas, pixelData, genislik, yukseklik) {
